c80324e0cb6dcee1f51be2cae66bfbf23e8825cf
Stalin-27 <rjstalin27@gmail.com>
2024-07-26 15:10:50 +0530

    hey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
     return (
       
             <div className="App">
+                
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
