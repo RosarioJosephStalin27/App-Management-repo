d75c1e68b98d50ddc286c9ef5a78bde6c5b6d019
Stalin-27 <153793848+Stalin-27@users.noreply.github.com>
2024-08-14 09:50:53 +0530

    Add files via upload

---
+++ dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.fitness.gym.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String email;
+    private String password;
+}
